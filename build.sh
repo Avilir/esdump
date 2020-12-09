a15c627ce3c17c93e4846227113fbf87bb09a92a
Avi Liani <alayani@redhat.com>
2020-12-09 15:20:57 +0200

    convert the repo name to argument
    Signed-off-by: Avi Liani <alayani@redhat.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 ##############################################################################
 #
-# This script is for building the esdump container.
+# This script is for building the esdump container and push it to repository.
 # it used the 'podman' for the build and accept the build TAG 
 # from the user.
 # without the tag the script will stop and exit with error.
@@ -17,19 +17,20 @@
 
 Tag=$1
 
-Repo="quay.io/avili/esdump"
-# This is the repository that the container will push into.
-# This need to be change if you want to use a different repository.
-#Repo="docker-registry.upshift.redhat.com/esdump/esdump"
-
-CMD_TOOL='podman'  # if you want this can be replace with : 'docker'
-
-
 if [[ ${Tag} == "" ]] ; then
 	echo "Error: you mast give the tag name for the build !"
 	exit 1
 fi
 
+Base_Repo=$2
+if [[ ${Base_Repo} == "" ]] ; then
+	echo "Error: you mast give the Base path for the repository!"
+	exit 1
+fi
+Repo="${Base_Repo}/esdump"
+
+CMD_TOOL='podman'  # if you want this can be replace with : 'docker'
+
 echo "Build the container image"
 ${CMD_TOOL} build -t ${Repo}:${Tag} .
-
+${CMD_TOOL} push ${Repo}:${Tag}
